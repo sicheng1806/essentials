ee57bced9462a7cac89a33935cc98cf7f6e8f513
sicheng1806 <sicheng1806@outlook.com>
2023-12-24 13:29:03 +0800

    Updata .gitignore

--- src_test/rust/rs_test/target/debug/deps/rs_test-d7d29c559ee897e3.d
+++
@@ -1,5 +0,0 @@
-/home/sicheng1806/script/python/sphinx/sicheng_essentials/src_test/rust/rs_test/target/debug/deps/rs_test-d7d29c559ee897e3: src/main.rs
-
-/home/sicheng1806/script/python/sphinx/sicheng_essentials/src_test/rust/rs_test/target/debug/deps/rs_test-d7d29c559ee897e3.d: src/main.rs
-
-src/main.rs:
--- src_test/rust/rs_test/target/debug/rs_test.d
+++
@@ -1,1 +0,0 @@
-/home/sicheng1806/script/python/sphinx/sicheng_essentials/src_test/rust/rs_test/target/debug/rs_test: /home/sicheng1806/script/python/sphinx/sicheng_essentials/src_test/rust/rs_test/src/main.rs
